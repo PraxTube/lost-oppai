53f221fb0f81b428069097d0d7b28eaf78f7a564
PraxTube <lukinator28@gmail.com>
2024-02-19 07:49:59 +0100

    fix!: Remove screen frame debugger
    It seems to cause panics when you toggle it off, I removed it for now,
    it might be fixed in Bevy 0.13

--- src/utils/debug.rs
+++ src/utils/debug.rs
@@ -1,19 +1,10 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
-use bevy_screen_diagnostics::{
-    ScreenDiagnostics, ScreenDiagnosticsPlugin, ScreenEntityDiagnosticsPlugin,
-    ScreenFrameDiagnosticsPlugin,
-};
 
-use crate::{player::input::PlayerInput, GameState};
+use crate::player::input::PlayerInput;
 
 #[derive(Resource, Default, Deref, DerefMut)]
 pub struct DebugActive(pub bool);
-
-#[derive(Resource, Default)]
-struct Diagnostics {
-    active: bool,
-}
 
 fn toggle_debug_mod(player_input: Res<PlayerInput>, mut debug_active: ResMut<DebugActive>) {
     if player_input.toggle_debug {
@@ -30,54 +21,11 @@ fn toggle_rapier_debug(
     }
 }
 
-fn toggle_diags(screen_diags: &mut ResMut<ScreenDiagnostics>) {
-    screen_diags.modify("fps").toggle();
-    screen_diags.modify("ms/frame").toggle();
-    screen_diags.modify("entities").toggle();
-}
-
-fn toggle_diagnostics_off(mut screen_diags: ResMut<ScreenDiagnostics>) {
-    if false {
-        toggle_diags(&mut screen_diags);
-    }
-}
-
-fn toggle_diagnostics(
-    debug_active: Res<DebugActive>,
-    mut diags: ResMut<Diagnostics>,
-    mut screen_diags: ResMut<ScreenDiagnostics>,
-) {
-    if **debug_active != diags.active {
-        diags.active = **debug_active;
-        toggle_diags(&mut screen_diags);
-    }
-}
-
 pub struct DebugPlugin;
 
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((
-            ScreenDiagnosticsPlugin {
-                timestep: 1.0,
-                style: Style {
-                    position_type: PositionType::Absolute,
-                    top: Val::Px(5.0),
-                    left: Val::Px(15.0),
-                    ..default()
-                },
-                ..default()
-            },
-            ScreenFrameDiagnosticsPlugin,
-            ScreenEntityDiagnosticsPlugin,
-        ))
-        .init_resource::<DebugActive>()
-        .init_resource::<Diagnostics>()
-        .add_systems(Update, (toggle_debug_mod, toggle_rapier_debug))
-        .add_systems(OnExit(GameState::AssetLoading), toggle_diagnostics_off)
-        .add_systems(
-            Update,
-            toggle_diagnostics.run_if(resource_changed::<DebugActive>()),
-        );
+        app.init_resource::<DebugActive>()
+            .add_systems(Update, (toggle_debug_mod, toggle_rapier_debug));
     }
 }
