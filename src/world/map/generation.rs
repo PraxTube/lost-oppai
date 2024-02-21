82dd2ad1de86a3f57bdd353cf76cd9f824d66169
PraxTube <lukinator28@gmail.com>
2024-02-21 14:13:47 +0100

    feat: Generate path based on bezier curve

--- src/world/map/generation.rs
+++ src/world/map/generation.rs
@@ -3,6 +3,8 @@ use rand::{thread_rng, Rng};
 use bevy::prelude::*;
 use noisy_bevy::simplex_noise_2d_seeded;
 
+use crate::GameState;
+
 use super::{CHUNK_SIZE, RENDERED_CHUNKS_RADIUS};
 
 const SEED: f32 = 60.0;
@@ -351,7 +353,8 @@ impl BitMap {
         let secondary_noise = simplex_noise_2d_seeded(w * NOISE_ZOOM, SEED + 1.0) * 1.0;
         let h = noise + secondary_noise;
 
-        let tile = if h < 0.0 { WATER } else { PRIMITIVE_GRASS };
+        // let tile = if h < 0.0 { WATER } else { PRIMITIVE_GRASS };
+        let tile = if h < 10.0 { WATER } else { PRIMITIVE_GRASS };
 
         self.set_tileset(v, tile);
         tile == WATER
@@ -416,10 +419,69 @@ impl BitMap {
     }
 }
 
+fn generate_bezier_curve_points(distance: f32, sample_size: usize) -> Vec<IVec2> {
+    let mut rng = thread_rng();
+    let x = rng.gen_range(-distance..distance);
+    let y_sign = if rng.gen_bool(0.5) { 1.0 } else { -1.0 };
+    let y = y_sign * (distance.powi(2) - x.powi(2)).sqrt();
+    let p1 = Vec2::ZERO;
+    let p2 = Vec2::new(x, y);
+
+    let min_p = Vec2::new(p1.x.min(p2.x), p1.y.min(p2.y));
+    let max_p = Vec2::new(p1.x.max(p2.x), p1.y.max(p2.y));
+    let c1 = Vec2::new(
+        rng.gen_range(min_p.x..max_p.x),
+        rng.gen_range(min_p.y..max_p.y),
+    );
+    let c2 = Vec2::new(
+        rng.gen_range(min_p.x..max_p.x),
+        rng.gen_range(min_p.y..max_p.y),
+    );
+
+    fn curve(p1: Vec2, p2: Vec2, c1: Vec2, c2: Vec2, t: f32) -> Vec2 {
+        (1.0 - t).powi(3) * p1
+            + 3.0 * t * (1.0 - t).powi(2) * c1
+            + 3.0 * t.powi(2) * (1.0 - t) * c2
+            + t.powi(3) * p2
+    }
+
+    let mut points = Vec::new();
+    for i in 0..sample_size {
+        let t = i as f32 / sample_size as f32;
+        let pos = curve(p1, p2, c1, c2, t);
+        let dis_pos = IVec2::new(pos.x as i32, pos.y as i32);
+        points.push(dis_pos);
+    }
+    points
+}
+
+fn generate_path(mut bitmap: ResMut<BitMap>) {
+    let distance = 100.0;
+    let sample_size = 100;
+    let width = 1;
+
+    info!("Generating path...");
+
+    let points = generate_bezier_curve_points(distance, sample_size);
+
+    info!("Filling up path...");
+
+    for v in points {
+        for i in -width..=width {
+            for j in -width..=width {
+                bitmap.set_tileset(v + IVec2::new(i, j), PRIMITIVE_GRASS);
+            }
+        }
+    }
+
+    info!("Done generating path.");
+}
+
 pub struct MapGenerationPlugin;
 
 impl Plugin for MapGenerationPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<BitMap>();
+        app.init_resource::<BitMap>()
+            .add_systems(OnExit(GameState::AssetLoading), (generate_path,));
     }
 }
