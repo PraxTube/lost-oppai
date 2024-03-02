202f355bbbf5639664711b40c05251d75e7c1bcf
PraxTube <lukinator28@gmail.com>
2024-03-02 12:37:28 +0100

    feat: Add multi paths (basic version)

--- src/world/map/generation/path.rs
+++ src/world/map/generation/path.rs
@@ -8,26 +8,13 @@ use crate::{GameRng, GameState};
 use super::{BitMap, GRASS_TYPE_INDEX, PATH_TYPE_INDEX};
 
 const NOISE_ZOOM: f32 = 0.02;
+const SAMPLE_RATE: f32 = 1.5;
+const MIN_RADIUS: i32 = 1;
+const MAX_RADIUS: i32 = MIN_RADIUS + 1;
+const MIN_RADIUS_GRASS: i32 = MAX_RADIUS + 1;
+const MAX_RADIUS_GRASS: i32 = MIN_RADIUS_GRASS + 3;
 
-fn generate_bezier_curve_points(distance: f32, sample_size: usize, seed: u64) -> Vec<IVec2> {
-    let mut rng = GameRng::seed_from_u64(seed);
-    let x = rng.gen_range(-distance..distance);
-    let y_sign = if rng.gen_bool(0.5) { 1.0 } else { -1.0 };
-    let y = y_sign * (distance.powi(2) - x.powi(2)).sqrt();
-    let p1 = Vec2::ZERO;
-    let p2 = Vec2::new(x, y);
-
-    let min_p = Vec2::new(p1.x.min(p2.x), p1.y.min(p2.y));
-    let max_p = Vec2::new(p1.x.max(p2.x), p1.y.max(p2.y));
-    let c1 = Vec2::new(
-        rng.gen_range(min_p.x..max_p.x),
-        rng.gen_range(min_p.y..max_p.y),
-    );
-    let c2 = Vec2::new(
-        rng.gen_range(min_p.x..max_p.x),
-        rng.gen_range(min_p.y..max_p.y),
-    );
-
+fn compute_path_points(p1: Vec2, p2: Vec2, c1: Vec2, c2: Vec2, sample_size: usize) -> Vec<IVec2> {
     fn curve(p1: Vec2, p2: Vec2, c1: Vec2, c2: Vec2, t: f32) -> Vec2 {
         (1.0 - t).powi(3) * p1
             + 3.0 * t * (1.0 - t).powi(2) * c1
@@ -45,27 +32,37 @@ fn generate_bezier_curve_points(distance: f32, sample_size: usize, seed: u64) ->
     points
 }
 
-fn generate_path(mut bitmap: ResMut<BitMap>) {
-    let distance = 200.0;
-    let sample_size = 200;
-    let min_radius: i32 = 1;
-    let max_radius: i32 = min_radius + 1;
-    let min_radius_grass: i32 = max_radius + 1;
-    let max_radius_grass: i32 = min_radius_grass + 3;
+fn generate_bezier_points(rng: &mut GameRng, p1: Vec2, distance: f32) -> (Vec2, Vec2, Vec2, Vec2) {
+    let x = rng.gen_range(-distance..distance);
+    let y_sign = if rng.gen_bool(0.5) { 1.0 } else { -1.0 };
+    let y = y_sign * (distance.powi(2) - x.powi(2)).sqrt();
+    let p2 = p1 + Vec2::new(x, y);
 
-    let points = generate_bezier_curve_points(distance, sample_size, bitmap.seed() as u64);
+    let min_p = Vec2::new(p1.x.min(p2.x), p1.y.min(p2.y));
+    let max_p = Vec2::new(p1.x.max(p2.x), p1.y.max(p2.y));
+    let c1 = Vec2::new(
+        rng.gen_range(min_p.x..max_p.x),
+        rng.gen_range(min_p.y..max_p.y),
+    );
+    let c2 = Vec2::new(
+        rng.gen_range(min_p.x..max_p.x),
+        rng.gen_range(min_p.y..max_p.y),
+    );
+    (p1, p2, c1, c2)
+}
 
+fn fill_path_points(bitmap: &mut ResMut<BitMap>, points: Vec<IVec2>) {
     for v in points {
         let w = Vec2::new(v.x as f32, v.y as f32);
 
         let noise = simplex_noise_2d_seeded(w * NOISE_ZOOM, bitmap.seed());
         let secondary_noise = simplex_noise_2d_seeded(w * NOISE_ZOOM, bitmap.seed() + 1.0) * 1.0;
-        let radius = (min_radius as f32
-            + 0.25 * (noise + secondary_noise + 2.0) * (max_radius - min_radius) as f32)
+        let radius = (MIN_RADIUS as f32
+            + 0.25 * (noise + secondary_noise + 2.0) * (MAX_RADIUS - MIN_RADIUS) as f32)
             as i32;
         let sqrt_radius = radius.pow(2);
-        let radius_grass = (min_radius_grass as f32
-            + 0.25 * (noise + secondary_noise + 2.0) * (max_radius_grass - min_radius_grass) as f32)
+        let radius_grass = (MIN_RADIUS_GRASS as f32
+            + 0.25 * (noise + secondary_noise + 2.0) * (MAX_RADIUS_GRASS - MIN_RADIUS_GRASS) as f32)
             as i32;
         let sqrt_radius_grass = radius_grass.pow(2);
 
@@ -82,6 +79,25 @@ fn generate_path(mut bitmap: ResMut<BitMap>) {
                 }
             }
         }
+    }
+}
+
+fn generate_path(mut bitmap: ResMut<BitMap>) {
+    let mut rng = GameRng::seed_from_u64(bitmap.seed() as u64);
+    let distance = 125.0;
+    let sample_size = (distance * SAMPLE_RATE) as usize;
+
+    let (p1, p2, c1, c2) = generate_bezier_points(&mut rng, Vec2::ZERO, distance);
+    let center_point = p2;
+    let points = compute_path_points(p1, p2, c1, c2, sample_size);
+    fill_path_points(&mut bitmap, points);
+
+    let distance = 80.0;
+    let sample_size = (distance * SAMPLE_RATE) as usize;
+    for _ in 0..3 {
+        let (p1, p2, c1, c2) = generate_bezier_points(&mut rng, center_point, distance);
+        let points = compute_path_points(p1, p2, c1, c2, sample_size);
+        fill_path_points(&mut bitmap, points);
     }
 }
 
