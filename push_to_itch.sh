d8e28089098969af52098a80976b055284b6f766
Rancic <lukinator28@gmail.com>
2024-08-03 13:41:10 +0200

    chore: Add itch butler sh script for atomic itch changes

---
+++ push_to_itch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# It is required to have itch `butler` installed.
+
+binary="lost-oppai"
+itch_target="PraxTube/lost-oppai"
+tag=$(git describe --tags --abbrev=0)
+
+rm -rf tmp_wasm
+cp -r wasm tmp_wasm
+
+cargo build --target wasm32-unknown-unknown --profile wasm
+wasm-bindgen --no-typescript --out-name bevy_game --out-dir tmp_wasm --target web target/wasm32-unknown-unknown/wasm/$binary.wasm
+
+cp -r assets tmp_wasm/
+cd tmp_wasm
+zip --recurse-paths $binary.zip .
+
+butler push \
+  --fix-permissions \
+  --userversion=$tag \
+  $binary.zip \
+  $itch_target:wasm
